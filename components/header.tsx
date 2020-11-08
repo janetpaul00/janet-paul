ba976ef275acf153ac4c38d67d19b402a3b95aa3
Ali Zahid <ali.zahid@live.com>
2020-11-08 13:05:54 +0500

    Styles updated

--- components/header.tsx
+++ components/header.tsx
@@ -4,14 +4,14 @@ import React, { FunctionComponent } from 'react'
 export const Header: FunctionComponent = () => (
   <header className="flex items-center justify-between mb-16">
     <Link href="/">
-      <a className="font-semibold text-xl mr-8">Janet Paul</a>
+      <a className="text-red-500 font-semibold text-xl mr-8">Janet Paul</a>
     </Link>
     <nav className="font-medium">
-      <a href="/Janet Paul.pdf" className="text-blue-500">
+      <a href="/Janet Paul.pdf" className="text-indigo-500">
         Resume
       </a>
       <Link href="/blog">
-        <a className="text-indigo-500 ml-4">Blog</a>
+        <a className="text-green-500 ml-4">Blog</a>
       </Link>
     </nav>
   </header>
